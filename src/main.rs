a6562b4425b2dae4f0e80dd6cf084de3311c37cf
octanuary <94416681+octanuary@users.noreply.github.com>
2024-10-09 23:06:00 -0500

    merge the constructor and child parse methods, fix namespaces

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use xml::name::Name;
 use xml::namespace::Namespace;
 use xml::writer::{EmitterConfig, XmlEvent};
 
-const BLOCK_PROPS:[&str; 4] = ["_bindings", "_watchers", "_bindingsByDestination", "_bindingsBeginWithWord"];
+const BLOCK_PROPS:[&str; 5] = ["_bindings", "_watchers", "_bindingsByDestination", "_bindingsBeginWithWord", "currentState"];
 const DEFAULT_NAMESPACES:&[DefaultNamespace] = &[
 	DefaultNamespace {
 		base_package: &"fx",
@@ -78,7 +78,158 @@ struct MxmlElement {
 	children: Vec<MxmlElement>
 }
 impl MxmlElement {
-	fn get_parser_events(&self, doc:&MxmlDoc) -> Result<Vec<XmlEvent>, Box<dyn Error>> {
+	/// parses a child element definition
+	fn parse(doc:&MxmlDoc, elem_def:&FunctionDefinition) -> Option<MxmlElement> {
+		let parse_as_root:bool = elem_def.name_identifier().0 == doc.class.name.0;
+
+		let body = elem_def.common.body.as_ref().unwrap();
+		let FunctionBody::Block(body) = body else {
+			return None;
+		};
+
+		let mut attributes = Vec::new();
+		let mut children = Vec::new();
+
+		let mut directives = body.directives.iter();
+		let mut elem_var_name:Option<String> = None;
+		if !parse_as_root {
+			let elem_init = directives.next();
+			match elem_init {
+				Some(elem_init) => {
+					if_chain! {
+						if let Directive::VariableDefinition(expr) = elem_init.as_ref();
+						if let Expression::QualifiedIdentifier(id) = expr.bindings.first().unwrap().destructuring.destructuring.as_ref();
+						then {
+							elem_var_name = Some(id.to_identifier_name_or_asterisk().unwrap().0);
+						}
+					}
+				}
+				None => return None
+			}
+		}
+		for expr in directives {
+			let Directive::ExpressionStatement(expr) = expr.as_ref() else {
+				continue;
+			};
+			match expr.expression.as_ref() {
+				Expression::Assignment(expr) => {
+					if let Expression::Member(left) = expr.left.as_ref() {
+						let id:(String, Location);
+						// only accept properties of `this`
+						if parse_as_root {
+							let Expression::ThisLiteral(_) = left.base.as_ref() else {continue};
+							id = left.identifier.to_identifier_name().unwrap();
+						} else {
+							let Expression::QualifiedIdentifier(qid) = left.base.as_ref() else {continue};
+							if qid.to_identifier_name().unwrap().0 != elem_var_name.as_ref().unwrap().clone() {continue}
+							id = left.identifier.to_identifier_name().unwrap();
+						}
+						
+						// children array
+						if id.0 == "mxmlContent" {
+							//TODO PARSE CHILDREn
+							let Expression::ArrayLiteral(right) = expr.right.as_ref() else {
+								continue;
+							};
+							let names = MxmlElement::child_func_names(right);
+							for func_name in names {
+								let function = doc
+									.get_function(&func_name)
+									.expect("child function is called in element definition but is not defined");
+								let elem = MxmlElement::parse(doc, function);
+								if elem.is_some() {
+									children.push(elem.unwrap());
+								}
+							}
+							continue;
+						}
+						// embeds
+						if id.0.contains("_embed_mxml_") {
+							if !parse_as_root {
+								continue;
+							}
+							// TOdo proper embed handler
+							continue;
+						}
+						// attributes OR properties
+						match ElemAttribute::parse(id.0.clone(), expr.right.as_ref()) {
+							Some(attr) => attributes.push(attr),
+							None => continue,
+						}
+						continue;
+					}
+					// states
+					if_chain! {
+						if let Expression::QualifiedIdentifier(left) = expr.left.as_ref();
+						if let QualifiedIdentifierIdentifier::Id(id) = &left.id;
+						if id.0 != "states";
+						if parse_as_root;
+						then {
+							// TODO PARE STAETS
+						} else {
+							continue;
+						}
+					}
+				}
+				// component declarations
+				Expression::Call(expr) => {
+					if !parse_as_root {
+						continue;
+					}
+				}
+				_ => {}
+			}
+		}
+		let class_name:String;
+		if parse_as_root {
+			let Expression::QualifiedIdentifier(extends) = doc.class.extends_clause.as_ref().unwrap().as_ref() else {
+				return None;
+			};
+			class_name = extends.to_identifier_name().unwrap().0.to_owned();
+		} else {
+			if_chain! {
+				if let Expression::QualifiedIdentifier(res_type) = elem_def
+					.common.signature.result_type
+					.as_ref()
+					.expect("no return type specified")
+					.as_ref();
+				if let Some(res_type) = res_type.to_identifier_name();
+				then {
+					class_name = res_type.0.to_owned();
+				} else {
+					// could not get element type
+					return None;
+				}
+			}
+		}
+		
+		Some(MxmlElement {
+			namespace: doc.use_namespace(&class_name),
+			class_name,
+			attributes,
+			children,
+		})
+	}
+
+	/// parses an array of this method calls and returns a vector of
+	/// the function names
+	fn child_func_names(array:&ArrayLiteral) -> Vec<String> {
+		let mut names = Vec::new();
+		for child in &array.elements {
+			if_chain! {
+				if let Element::Expression(child) = child;
+				if let Expression::Call(call) = child.as_ref();
+				if let Expression::Member(member) = call.base.as_ref();
+				if let QualifiedIdentifierIdentifier::Id(id) = &member.identifier.id;
+				then {
+					names.push(id.0.to_owned());
+				}
+			}
+		}
+		names
+	}
+
+	fn get_writer_events(&self, doc:&MxmlDoc) -> Result<Vec<XmlEvent>, Box<dyn Error>> {
 		let mut attributes = Vec::new();
 
 		for attribute in &self.attributes {
@@ -95,7 +246,7 @@ impl MxmlElement {
 		// add namespaces if we're on the root element
 		let namespace:Namespace;
 		if doc.root.as_ref().unwrap() == self {
-			namespace = doc.namespaces.to_owned();
+			namespace = doc.namespaces.borrow().to_owned();
 		} else {
 			namespace = Namespace::empty();
 		}
@@ -112,6 +263,14 @@ impl MxmlElement {
 
 		let mut events:Vec<XmlEvent> = Vec::new();
 		events.push(start.into());
+
+		for child in &self.children {
+			let mut child_events = child
+				.get_writer_events(doc)
+				.expect("Should have been able to get child events");
+			events.append(&mut child_events);
+		}
+
 		events.push(XmlEvent::end_element().into());
 		Ok(events)
 	}
@@ -124,10 +283,11 @@ struct Binding {
 }
 
 struct MxmlDoc {
+	name: Vec<String>,
 	imports: Vec<ImportDirective>,
 	class: ClassDefinition,
 	root: Option<MxmlElement>,
-	namespaces: Namespace,
+	namespaces: RefCell<Namespace>,
 	bindings: Vec<Binding>,
 }
 impl MxmlDoc {
@@ -151,19 +311,21 @@ impl MxmlDoc {
 					}
 
 					let mut doc = MxmlDoc {
+						name: MxmlDoc::package_name_to_vec(&package.name),
 						imports,
 						class: class.to_owned(),
 						root: None,
-						namespaces: namespace,
+						namespaces: RefCell::new(namespace),
 						bindings: Vec::new(),
 					};
-					if !doc.is_valid_doc() {
+					let constructor = doc.get_function(&doc.class.name.0).unwrap();
+					if !MxmlDoc::is_doc_elem(constructor) {
 						return None;
 					}
 					// store the bindings first so we can use them during element parsing
 					// doc.bindings
 		
-					doc.root = doc.parse_constructor();
+					doc.root = MxmlElement::parse(&doc, constructor);
 					return Some(doc);
 				},
 				_ => continue,
@@ -186,6 +348,13 @@ impl MxmlDoc {
 		None
 	}
 
+	fn package_name_to_vec(name: &Vec<(String, Location)>) -> Vec<String> {
+		name.to_owned()
+			.iter_mut()
+			.map(|v| return v.0.to_owned())
+			.collect::<Vec<String>>()
+	}
+
 	/// returns the name of an imported package as a vector of strings
 	fn get_package_name(&self, specifier:&String) -> Option<Vec<String>> {
 		for import in self.imports.iter() {
@@ -193,22 +362,19 @@ impl MxmlDoc {
 				if let ImportSpecifier::Identifier(id) = &import.import_specifier;
 				if &id.0 == specifier;
 				then {
-					let name = 
-						import.package_name.to_owned()
-						.iter_mut()
-						.map(|v| return v.0.to_owned())
-						.collect::<Vec<String>>();
-					return Some(name.to_owned());
+					let name = MxmlDoc::package_name_to_vec(&import.package_name);
+					return Some(name);
 				}
 			}
 		}
-		None
+		// no import statement found, default to component directory
+		Some(self.name.clone())
 	}
 
-	fn use_namespace(&mut self, specifier:&String) -> String {
+	fn use_namespace(&self, specifier:&String) -> String {
 		let full_name = self
 			.get_package_name(specifier)
-			.expect("Import should have existed");
+			.expect(("Import for ".to_owned() + specifier + " should have existed").as_str());
 
 		let first_val = full_name
 			.first()
@@ -220,82 +386,14 @@ impl MxmlDoc {
 
 		let name = full_name.last().unwrap().to_owned();
 		let value = full_name.join(".") + ".*";
-		self.namespaces.put(name.clone(), value);
+		self.namespaces.borrow_mut().put(name.clone(), value);
 		name
 	}
 
-	fn parse_constructor(&mut self) -> Option<MxmlElement> {
-		let constructor = self.get_function(&self.class.name.0).unwrap();
-		let body = constructor.common.body.as_ref().unwrap();
-		let FunctionBody::Block(body) = body else {
-			return None;
-		};
-		let mut attributes:Vec<ElemAttribute> = Vec::new();
-		for expr in body.directives.iter() {
-			let Directive::ExpressionStatement(expr) = expr.as_ref() else {
-				continue;
-			};
-			match expr.expression.as_ref() {
-				Expression::Assignment(expr) => {
-					if_chain! {
-						if let Expression::Member(left) = expr.left.as_ref();
-						// only accept properties of `this`
-						if let Expression::ThisLiteral(_) = left.base.as_ref();
-						if let QualifiedIdentifierIdentifier::Id(id) = &left.identifier.id;
-						then {
-							// children array
-							if id.0 == "mxmlContent" {
-								//TODO PARSE CHILDREn
-								continue;
-							}
-							// embeds
-							if id.0.contains("_embed_mxml_") {
-								// TOdo proper embed handler
-								continue;
-							}
-							// attributes OR properties
-							match ElemAttribute::parse(id.0.clone(), expr.right.as_ref()) {
-								Some(attr) => attributes.push(attr),
-								None => continue,
-							}
-							continue;
-						}
-					}
-					// states
-					if_chain! {
-						if let Expression::QualifiedIdentifier(left) = expr.left.as_ref();
-						if let QualifiedIdentifierIdentifier::Id(id) = &left.id;
-						if id.0 != "states";
-						then {
-							// TODO PARE STAETS
-						} else {
-							continue;
-						}
-					}
-				}
-				// component declarations
-				Expression::Call(expr) => {
-
-				}
-				_ => {}
-			}
-		}
-		let Expression::QualifiedIdentifier(extends) = self.class.extends_clause.as_ref().unwrap().as_ref() else {
-			return None;
-		};
-		let class_name = extends.to_identifier_name().unwrap().0.to_owned();
-		Some(MxmlElement {
-			namespace: self.use_namespace(&class_name),
-			class_name,
-			attributes,
-			children: Vec::new(),
-		})
-	}
-
-	/// checks for an `mx_internal::_document = this;` statement in the constructor
-	fn is_valid_doc(&self) -> bool {
-		let constructor = self.get_function(&self.class.name.0).unwrap();
-		let body = constructor.common.body.as_ref().unwrap();
+	/// checks for an `mx_internal::_document = this;` statement
+	/// in an element definition
+	fn is_doc_elem(elem_def:&FunctionDefinition) -> bool {
+		let body = elem_def.common.body.as_ref().unwrap();
 		let FunctionBody::Block(body) = body else {
 			return false;
 		};
@@ -341,7 +439,7 @@ impl MxmlDoc {
 			.create_writer(stdout);		
 
 		let root = self.root.as_ref().unwrap();
-		let events = root.get_parser_events(self)?;
+		let events = root.get_writer_events(self)?;
 		for event in events {
 			writer.write(event)?;
 		}
